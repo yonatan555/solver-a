c855045ad7d5e2cc44b2342d17120ea685071b66
yonatan555 <57701623+yonatan555@users.noreply.github.com>
2020-04-29 10:36:56 +0300

    Add files via upload

---
+++ cmake-build-debug/CMakeFiles/solver_a.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/yonatan/CLionProjects/solver-a/Demo.cpp" "/home/yonatan/CLionProjects/solver-a/cmake-build-debug/CMakeFiles/solver_a.dir/Demo.cpp.o"
+  "/home/yonatan/CLionProjects/solver-a/TestCounter.cpp" "/home/yonatan/CLionProjects/solver-a/cmake-build-debug/CMakeFiles/solver_a.dir/TestCounter.cpp.o"
+  "/home/yonatan/CLionProjects/solver-a/solver.cpp" "/home/yonatan/CLionProjects/solver-a/cmake-build-debug/CMakeFiles/solver_a.dir/solver.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../."
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/solver_a.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/solver_a.dir/Demo.cpp.o"
+  "CMakeFiles/solver_a.dir/TestCounter.cpp.o"
+  "CMakeFiles/solver_a.dir/solver.cpp.o"
+  "solver_a"
+  "solver_a.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/solver_a.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
